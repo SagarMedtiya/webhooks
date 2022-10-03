a72b8280c276fe98ae6f78e2832c355b6dbdf1ba
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2022-10-03 22:59:43 +0530

    edit the file

--- src/App.js
+++ src/App.js
@@ -2,8 +2,52 @@ import './App.css';
 import {useState} from 'react';
 import axios from 'axios';
 
+
+const eventsList =[
+    {
+        key: 'COMMIT',
+        title:'Commit',
+        checked: 'true',
+    },
+    {
+        key: 'PUSH',
+        title:'Push',
+        checked: 'false',
+    },
+    {
+        key: 'MERGE',
+        title: 'Merge',
+        checked: 'false',
+    }
+];
+
 function App() {
-  return (
+    const [formData, setForData] = useState({
+        payloadUrl: '',
+        secret: '',
+        eventTypes: [...eventsList],
+    });
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        axios.post('/api/webhooks',{
+            ...formData,
+            eventTypes: formData.eventTypes
+                .filter((item)=> !!item.checked)
+                .map((item)=>item.key),
+        })
+    };
+    const handleFormChange=(e,key)=>{
+        if(e.target.name === 'eventTypes'){
+            setFormData((prev)=>{
+                const checkboxes = prev[e.target.name].map((item)=>{
+                    return (
+                        
+                    )
+                })
+            })
+        }
+    }
+    return (
     <div></div>
   );
 }
