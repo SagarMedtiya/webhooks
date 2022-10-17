73e67f85ef6a8d2a2e4edabdc98270cf7a1c5b7d
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2022-10-17 23:20:56 +0530

    controller js file

---
+++ backend/controller/controller.js
@@ -0,0 +1,32 @@
+const webhooks= {
+    COMMIT: [],
+    PUSH:[],
+    MERGE:[]
+}
+
+class controller{
+
+    async webhooks(req,res){
+        
+        const {payloadUrl, secret, eventTypes} = req.body;
+        eventTypes.forEach(eventType=>{
+            webhooks[eventType].push({payloadUrl: payloadUrl, secret: secret})
+        })
+        return res.sendStatus(201);
+    }
+    emulate(req,res){
+        const {type, data} = req.body;
+        //business logic comes here
+        
+        //event trigger 
+        setTimeout(()=>{
+
+        },1000)
+
+        res.sendStatus(200);
+
+    }
+}
+
+
+module.exports = new controller();
