3376ea7011b43a84f524288c126ced298fab2410
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2022-11-03 19:09:41 +0530

    add get route

--- backend/routes.js
+++ backend/routes.js
@@ -3,5 +3,7 @@ const controller = require("./controller/controller")
 
 router.post("/api/webhooks",controller.webhooks);
 router.post("/api/event-emulate", controller.emulate);
-
+router.get('/api/db',(req,res)=>{
+    res.json()
+})
 module.exports = router;
