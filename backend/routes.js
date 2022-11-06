1a0009d162fad568ef66d95aa28b051420f1810f
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2022-11-06 16:26:46 +0530

    add db route

--- backend/routes.js
+++ backend/routes.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
 const controller = require("./controller/controller")
 
+
 router.post("/api/webhooks",controller.webhooks);
 router.post("/api/event-emulate", controller.emulate);
-router.get('/api/db',(req,res)=>{
-    res.json()
-})
+router.get('/api/db',controller.db);
+
 module.exports = router;
