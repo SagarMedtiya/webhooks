9383e1dc2d56ea85920a8438e917376ebedbcaa6
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2022-11-29 23:54:46 +0530

    fix the bugs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -83,6 +83,7 @@ function App() {
                         id="payloadUrl"
                         className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
                         name="payloadUrl"
+                        
                     />
                 </div>
                 <div className='mt-4'>
