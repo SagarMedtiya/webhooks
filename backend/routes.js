2c19c2743fc01ddac24cae777e584aa7b10c1892
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2022-12-03 22:37:09 +0530

    fix the bugs

--- backend/routes.js
+++ backend/routes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const controller = require("./controller/controller")
 
 
-router.post("/api/webhooks",controller.webhooks);
+router.post("/api/webhooks",controller.webhookss);
 router.post("/api/event-emulate", controller.emulate);
 router.get('/api/db',controller.db);
 
