908f77e8405114b4e8d8ac6b951dc636e4f1483c
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2022-12-03 22:47:53 +0530

    add the home URL

--- custom-dashboard-backend/index.js
+++ custom-dashboard-backend/index.js
@@ -3,7 +3,19 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+app.use(express.json());
+
+const messages = [];
+app.post('/git-info',(req,res)=>{
+    const data = req.body;
+    messages.push(data);
+    res.sendStatus(200)
+})
 
 
+app.get('/',(req,res)=>{
+    return res.json(messages);
+    
+})
 
 app.listen(PORT, ()=>console.log(`Listening on ${PORT}`));
