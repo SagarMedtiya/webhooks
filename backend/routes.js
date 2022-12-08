e184f8c11a5c53a7bdd3e5eadc3ccd5564730caf
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2022-12-08 19:42:47 +0530

    create webhoook const

--- backend/routes.js
+++ backend/routes.js
@@ -1,6 +1,10 @@
 const router = require("express").Router();
 const controller = require("./controller/controller")
-
+const webhooks= {
+    COMMIT: [],
+    PUSH:[],
+    MERGE:[]
+}
 
 router.post("/api/webhooks",controller.webhookss);
 router.post("/api/event-emulate", controller.emulate);
